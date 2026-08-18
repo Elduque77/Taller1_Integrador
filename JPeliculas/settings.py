514a5b2f18a217772d3f96f30f4ca4d303fa43b2
Elduque77 <jfduqueg1@eafit.edu.co>
2026-08-18 16:23:20 +0000

    renderizar genre, year e imagen estatica del navbar

--- JPeliculas/settings.py
+++ JPeliculas/settings.py
@@ -12,17 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
-# Archivos estáticos (CSS, JS, Logos)
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'movie/static'),
-]
-
-# Archivos de medios (imágenes subidas por modelos/CSV)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,4 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
+# Archivos estáticos (CSS, JS, Logos)
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'movie/static'),
+]
+
+# Archivos de medios (imágenes subidas por modelos/CSV)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
